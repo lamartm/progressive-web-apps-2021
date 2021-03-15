b322363d9b07a803eba4926b577d5b6d25553aea
Mohamed <spider_mo@hotmail.nl>
2021-03-15 18:00:04 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const port = 3000
+
+const { getData } = require('./public/js/api')
+// const { loading } = require('./public/js/loader')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+app.use(express.static('public'))
+
+app.get('/', function(req, res) {
+	res.render('home', {
+		pageTitle: `Home`
+	})
+})
+
+app.get('/Curiosity', function(req, res) {
+	// loading('active')
+	getData('Curiosity')
+		.then(d => {
+			res.render('overview', { data: d } )
+		})
+})
+
+app.get('/Curiosity/:id', function(req, res) {
+	getData('Curiosity')
+		.then(d => {
+			res.render('detail', { 
+				data: d,
+				selectedImage: parseInt(req.params.id)
+			} )
+		})
+})
+
+app.get('/Spirit', function(req, res) {
+	getData('Spirit')
+		.then(d => {
+			res.render('overview', { data: d } )
+		})
+})
+
+app.get('/Opportunity', function(req, res) {
+	getData('Opportunity')
+		.then(d => {
+			res.render('overview', { data: d } )
+		})
+})
+
+app.listen(port, function() {
+	console.log(`Application started on port: ${port}`)
+})
---
+++ public/js/loader.js
@@ -0,0 +1,10 @@
+// function loading(state) {
+//     const loader = document.querySelector('div.loader')
+//     if (state === 'active') {
+//       loader.classList.add('loading')
+//     } else {
+//       loader.classList.remove('loading')
+//     }
+//   }
+
+// module.exports = {loading}
