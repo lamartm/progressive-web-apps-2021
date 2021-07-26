1905b0bd15c25192464317ca4627570ebf26dabe
Mohamed <spider_mo@hotmail.nl>
2021-07-26 23:46:37 +0200

    new vers

--- index.js
+++ index.js
@@ -1,84 +1,78 @@
-const express = require('express')
-const compression = require('compression')
-const port = process.env.PORT || 3000
+const express = require("express");
+const compression = require("compression");
+const port = process.env.PORT || 3000;
 
-const { getData } = require('./public/js/api')
-const cookieParser = require('cookie-parser')
+const { getData } = require("./public/js/api");
+const cookieParser = require("cookie-parser");
 
-const app = express()
+const app = express();
 
-app.use(compression({
-	level: 6,
-	threshold: 0
-})) 
-app.use(cookieParser())
-app.use(express.urlencoded({ extended: true }))
+app.use(
+  compression({
+    level: 6,
+    threshold: 0,
+  })
+);
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 
-app.set('view engine', 'ejs')
-app.set('views', 'views')
-app.use(express.static('public'))
+app.set("view engine", "ejs");
+app.set("views", "views");
+app.use(express.static("public"));
 
+app.get("/", function (req, res) {
+  res.render("home", {
+    pageTitle: `Home`,
+  });
+});
 
-app.get('/', function(req, res) {
-	res.render('home', {
+app.get("/Curiosity", function (req, res) {
+  // loading('active')
+  getData("Curiosity").then((d) => {
+    res.render("overview", { data: d });
+  });
+});
 
-		pageTitle: `Home`
-	})
-})
+app.get("/Curiosity/:id", function (req, res) {
+  getData("Curiosity").then((d) => {
+    res.render("detail", {
+      data: d,
+      selectedImage: parseInt(req.params.id),
+    });
+  });
+});
 
-app.get('/Curiosity', function(req, res) {
-	// loading('active')
-	getData('Curiosity')
-		.then(d => {
-			res.render('overview', { data: d } )
-		})
-})
+app.get("/Perseverance", function (req, res) {
+  // loading('active')
+  getData("Perseverance").then((d) => {
+    res.render("overview", { data: d });
+  });
+});
 
-app.get('/Curiosity/:id', function(req, res) {
-	getData('Curiosity')
-		.then(d => {
-			res.render('detail', { 
-				data: d,
-				selectedImage: parseInt(req.params.id)
-			} )
-		})
-})
+app.get("/Perseverance/:id", function (req, res) {
+  getData("Perseverance").then((d) => {
+    res.render("detail", {
+      data: d,
+      selectedImage: parseInt(req.params.id),
+    });
+  });
+});
 
-app.get('/Perseverance', function(req, res) {
-	// loading('active')
-	getData('Perseverance')
-		.then(d => {
-			res.render('overview', { data: d } )
-		})
-})
+app.get("/Spirit", function (req, res) {
+  getData("Spirit").then((d) => {
+    res.render("overview", { data: d });
+  });
+});
 
-app.get('/Perseverance/:id', function(req, res) {
-	getData('Perseverance')
-		.then(d => {
-			res.render('detail', { 
-				data: d,
-				selectedImage: parseInt(req.params.id)
-			} )
-		})
-})
+app.get("/Opportunity", function (req, res) {
+  getData("Opportunity").then((d) => {
+    res.render("overview", { data: d });
+  });
+});
+app.get("/offline", function (req, res) {
+  res.render("offline");
+});
 
-app.get('/Spirit', function(req, res) {
-	getData('Spirit')
-		.then(d => {
-			res.render('overview', { data: d } )
-		})
-})
-
-app.get('/Opportunity', function(req, res) {
-	getData('Opportunity')
-		.then(d => {
-			res.render('overview', { data: d } )
-		})
-})
-app.get('/offline', function(req, res) {
-			res.render('offline')
-})
-
-app.listen(port, function() {
-	console.log(`Application started on port: ${port}`)
-})
+app.listen(port, function () {
+  console.log(`Application started on port: ${port}`);
+});
--- public/sw.js
+++ public/sw.js
@@ -1,57 +1,58 @@
-const staticCache = 'static-site-v4'
-const dynamicCache = 'dynamic-cache-v5'
+const staticCache = "static-site-v13";
+const dynamicCache = "dynamic-cache-v32";
 const assets = [
-    'css/style.css',
-    'images/curiosity.jpg',
-    'images/opp.jpg',
-    'images/perseverance.jpg',
-    'images/spirit.jpg',
-    '/',
-    'js/api.js',
-    '/offline'
-]
-self.addEventListener('install', event => {
-    // console.log('service worker installed')
-    event.waitUntil(
-        caches.open(staticCache)
-            .then(cache =>{
-                console.log('caching')
-                cache.addAll(assets)
+  "images/curiosity.jpg",
+  "images/opp.jpg",
+  "images/perseverance.jpg",
+  "images/spirit.jpg",
+  "/",
+  "js/api.js",
+  "/offline",
+];
+self.addEventListener("install", (event) => {
+  // console.log('service worker installed')
+  event.waitUntil(
+    caches.open(staticCache).then((cache) => {
+      console.log("caching");
+      cache.addAll(assets);
     })
-    )
-}) 
+  );
+});
 
-self.addEventListener('activate', event => {
-    // console.log('service worker activated')
-    event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(keys
-                .filter(key => key !== staticCache && key !== dynamicCache)
-                .map(key => caches.delete(key)))
-        })
-    )
-})  
+self.addEventListener("activate", (event) => {
+  // console.log('service worker activated')
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== staticCache && key !== dynamicCache)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+});
 
-self.addEventListener('fetch', event => {
-    // console.log('service worker fetch', event)
-    event.respondWith(
-        caches.match(event.request)
-            .then(cacheRes => {
+self.addEventListener("fetch", (event) => {
+  // console.log('service worker fetch', event)
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((cacheRes) => {
+        return (
+          cacheRes ||
+          fetch(event.request).then((fetchRes) => {
+            return caches
+              .open(dynamicCache)
 
-                return cacheRes || fetch(event.request)
-
-            .then(fetchRes => {
-
-                return caches.open(dynamicCache)
-
-            .then(cache => {
-                 cache.put(event.request.url, fetchRes.clone())
-                return fetchRes
-                    })
-                })
-            })
-            .catch((err) => {
-                        return caches.match('/offline')
-                    })
-    )
-})  
+              .then((cache) => {
+                cache.put(event.request.url, fetchRes.clone());
+                return fetchRes;
+              });
+          })
+        );
+      })
+      .catch((err) => {
+        return caches.match("/offline");
+      })
+  );
+});
